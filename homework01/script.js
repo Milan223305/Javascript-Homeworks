dbadbf4021eb8f69ce85647efc23eeabb94eeb23
Milan223305 <123005628+Milan223305@users.noreply.github.com>
2024-12-01 18:48:01 +0100

    Add files via upload

---
+++ homework01/script.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program that will calculate the price of 30 Phones, where the price of one phone is $119.95 and the tax rate is 5%.
+
+let price = 119.95;
+
+let calculatePrice = 30 * price * 1.05;
+
+console.log(calculatePrice);
