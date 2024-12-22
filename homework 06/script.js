f784ce895f2e1af2f133296fc9b33afbc8dbcf49
Milan223305 <123005628+Milan223305@users.noreply.github.com>
2024-12-22 20:44:35 +0100

    Homework 6

---
+++ homework 06/script.js
@@ -0,0 +1,42 @@
+// HOMEWORK Part 1
+
+// Create OBJECT animal with 2 properties and 1 method:
+// name
+// kind
+// speak (method)
+// this method will take one parameter and will print in the console a message: e.g. dog.speak(“hey bro!!!”) will log in the console “Dog says: ‘Hey bro!!!’”
+
+// Bonus: enter the values from prompt or from HTML inputs
+
+// let object = {
+//   name: prompt("enter name:"),
+//   kind: prompt("Enter kind"),
+//   speak: prompt("What is it speaking"),
+//   speaking: function () {
+//     console.log(`The ${this.name} says '${this.speak}'`);
+//   },
+// };
+// object.speaking();
+
+// HOMEWORK Part 2
+// Write a JavaScript program to display the reading status of some book. The object should have the next properties: title, author, readingStatus and a method that will return info depending on the readingStatus e.g.
+
+// Already read 'The Robots of dawn' by Isaac Asimov. (if true)
+// You still need to read 'Mockingjay: The Final Book of The Hunger Games' by Suzanne Collins. (if false).
+// Bonus: enter the values from prompt() or read them from HTML
+
+// let book = {
+//   title: prompt("Enter title: "),
+//   author: prompt("Enter author"),
+//   readingStatus: prompt("Have you read thsi book? y/n"),
+//   status: function () {
+//     if (this.readingStatus === "y") {
+//       console.log(`Already read '${this.title}' by '${this.author}'`);
+//     } else if (this.readingStatus === "n") {
+//       console.log(`You still need to read '${this.title}' by '${this.author}'`);
+//     } else {
+//       console.log("Not valid, please enter y or n");
+//     }
+//   },
+// };
+// book.status();
